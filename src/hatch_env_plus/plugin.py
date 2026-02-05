1f24f1afe039dae4fdc3d269475aae48b105a064
Jens Keiner <jens.keiner@gmail.com>
2026-02-05 08:05:57 +0100

    Fix embarrassing bug when fallback is not used when environment variable is set to empty string.

--- src/hatch_env_plus/plugin.py
+++ src/hatch_env_plus/plugin.py
@@ -4,13 +4,13 @@ from hatchling.version.source.plugin.interface import VersionSourceInterface
 
 
 class EnvironmentVariableVersionSource(VersionSourceInterface):
-    PLUGIN_NAME = 'env-plus'
+    PLUGIN_NAME = "env-plus"
 
     def get_version_data(self) -> dict:
-        variable = self.config.get('variable', 'PACKAGE_VERSION')
-        fallback = self.config.get('fallback', '0.0.0dev0') or None
-        version = os.environ.get(variable, fallback)
-        return {'version': version}
+        variable = self.config.get("variable", "PACKAGE_VERSION")
+        fallback = self.config.get("fallback", "0.0.0dev0") or None
+        version = os.environ.get(variable, None) or fallback
+        return {"version": version}
 
     def set_version(self, version: str, version_data: dict) -> None:
-        version_data['version'] = version
+        version_data["version"] = version
